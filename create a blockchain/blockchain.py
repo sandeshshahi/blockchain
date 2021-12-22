036ab2988e32c361623bbcda95ba2113cbba9b13
sandeshshahi <sndshshahi@gmail.com>
2021-12-22 19:41:49 +0545

    ggetting the full blockchain

--- create a blockchain/blockchain.py
+++ create a blockchain/blockchain.py
@@ -87,6 +87,14 @@ def mine_block():
                 'previous_hash': block['previous_hash']
                 }
     return jsonify(response), 200
+
+# Getting the full Blockchain displayed in userinterface postman
+@app.route('/get_chain' , methods = ['GET'])
+def get_chain():
+    response = {'chain': blockchain.chain,
+                'length': len(blockchain.chain)
+                }
+    return jsonify(response), 200
            
        
        
